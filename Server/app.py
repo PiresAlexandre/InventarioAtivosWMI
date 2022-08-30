030f2ece792efe2e88b6e08911b1a0cc666c86ab
PiresAlexandre <myemail>
2022-08-30 22:32:38 +0100

    CORSError

--- Server/app.py
+++ Server/app.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from resources.routes import initialize_routes
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from os import environ
 
 
@@ -19,6 +20,7 @@ app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
 api = Api(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+cors = CORS(app)
 
 initialize_db(app)
 
